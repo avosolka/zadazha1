29392be668debc33713fad6f83091f0e32e6466f
Kolosova Zarema <kolosova.zarema@mail.ru>
2022-07-16 19:56:11 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int a = 1001;
+        int bonus = a/20;
+        System.out.println("количество бонусов:" + bonus);
+    }
+}
+
